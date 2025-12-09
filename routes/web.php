44df30592b2d99287bf31097531588d9a7276504
mary luz puita <maryluzpuitaquina222@gmail.com>
2025-12-09 15:34:44 -0400

    Feature: filtro de productos con stock > 5

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use App\Http\Controllers\ProductoController;
+Route::get('/productos/stock-alto', [ProductoController::class, 'stockAlto'])
+    ->name('productos.stock.alto');
 
 Route::resource('productos', ProductoController::class);
-
 
 Route::get('/', function () {
     return view('welcome');
